9dc6c6da7536273622ed69ca46d00f072957c71b
Seefzi <seefzi@outlook.com>
2026-01-03 03:25:28 -0600

    added the pacman stuff

--- user.sh
+++ user.sh
@@ -3,3 +3,14 @@ if [ "$SHELL" != "/bin/fish" ]; then
   chsh -s /bin/fish
 fi
 
+echo "==> Configuring pacman"
+
+PACMAN_CONF="/etc/pacman.conf"
+
+sudo sed -i 's/^#Color/Color/' "$PACMAN_CONF"
+sudo sed -i 's/^#VerbosePkgLists/VerbosePkgLists/' "$PACMAN_CONF"
+
+if ! grep -q '^ILoveCandy' "$PACMAN_CONF"; then
+  sudo sed -i '/^\[options\]/a ILoveCandy' "$PACMAN_CONF"
+fi
+
