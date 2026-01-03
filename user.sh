9b3c9f5a99058ae6ba28c79f1585839120f7bfe8
Seefzi <seefzi@outlook.com>
2026-01-03 03:28:36 -0600

    added the pacman stuff

--- user.sh
+++ user.sh
@@ -3,14 +3,23 @@ if [ "$SHELL" != "/bin/fish" ]; then
   chsh -s /bin/fish
 fi
 
-echo "==> Configuring pacman"
-
+# ---- pacman parallel downloads ----
 PACMAN_CONF="/etc/pacman.conf"
 
+echo "==> Configuring pacman performance options"
+
+# Uncomment Color + VerbosePkgLists if present
 sudo sed -i 's/^#Color/Color/' "$PACMAN_CONF"
 sudo sed -i 's/^#VerbosePkgLists/VerbosePkgLists/' "$PACMAN_CONF"
 
+# Enable ParallelDownloads (set to 5 if commented or missing)
+if grep -q '^#ParallelDownloads' "$PACMAN_CONF"; then
+  sudo sed -i 's/^#ParallelDownloads.*/ParallelDownloads = 5/' "$PACMAN_CONF"
+elif ! grep -q '^ParallelDownloads' "$PACMAN_CONF"; then
+  sudo sed -i '/^\[options\]/a ParallelDownloads = 5' "$PACMAN_CONF"
+fi
+
+# Enable ILoveCandy
 if ! grep -q '^ILoveCandy' "$PACMAN_CONF"; then
   sudo sed -i '/^\[options\]/a ILoveCandy' "$PACMAN_CONF"
 fi
-
